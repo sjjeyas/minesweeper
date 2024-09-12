152d0f93e6847dffe464e374ccb5b37051c025eb
Sneha Jeyasingh <sjeyasin@usc.edu>
2024-09-11 18:32:57 -0700

    grid added

--- app/src/main/java/com/example/project1_310/MainActivity.java
+++ app/src/main/java/com/example/project1_310/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.project1_310;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import androidx.gridlayout.widget.GridLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private int clock = 0;
-    private boolean running = false;
+    private boolean running = true;
+    private static final int COLUMN_COUNT = 2;
+
+    // save the TextViews of all cells in an array, so later on,
+    // when a TextView is clicked, we know which cell it is
+    private ArrayList<TextView> cell_tvs;
+
+    private int dpToPixel(int dp) {
+        float density = Resources.getSystem().getDisplayMetrics().density;
+        return Math.round(dp * density);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,8 +34,32 @@ public class MainActivity extends AppCompatActivity {
             clock = savedInstanceState.getInt("clock");
             running = savedInstanceState.getBoolean("running");
         }
-
         runTimer();
+
+        cell_tvs = new ArrayList<TextView>();
+        GridLayout grid = (GridLayout) findViewById(R.id.gridLayout01);
+        for (int i = 2; i<=3; i++) {
+            for (int j = 0; j <= 1; j++) {
+                TextView tv = new TextView(this);
+                tv.setHeight(dpToPixel(64));
+                tv.setWidth(dpToPixel(64));
+                tv.setTextSize(32);//dpToPixel(32) );
+                tv.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
+                tv.setTextColor(Color.GRAY);
+                tv.setBackgroundColor(Color.GRAY);
+                tv.setOnClickListener(this::onClickTV);
+
+                GridLayout.LayoutParams lp = new GridLayout.LayoutParams();
+                lp.setMargins(dpToPixel(2), dpToPixel(2), dpToPixel(2), dpToPixel(2));
+                lp.rowSpec = GridLayout.spec(i);
+                lp.columnSpec = GridLayout.spec(j);
+
+                grid.addView(tv, lp);
+
+                cell_tvs.add(tv);
+            }
+        }
+
     }
     public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
@@ -45,10 +86,7 @@ public class MainActivity extends AppCompatActivity {
         handler.post(new Runnable() {
             @Override
             public void run() {
-                int hours =clock/3600;
-                int minutes = (clock%3600) / 60;
-                int seconds = clock%60;
-                String time = String.format("%d:%02d:%02d", hours, minutes, seconds);
+                String time = String.format("%3d", clock);
                 timeView.setText(time);
 
                 if (running) {
@@ -57,5 +95,27 @@ public class MainActivity extends AppCompatActivity {
                 handler.postDelayed(this, 1000);
             }
         });
+    }
+    private int findIndexOfCellTextView(TextView tv) {
+        for (int n=0; n<cell_tvs.size(); n++) {
+            if (cell_tvs.get(n) == tv)
+                return n;
+        }
+        return -1;
+    }
+
+    public void onClickTV(View view){
+        TextView tv = (TextView) view;
+        int n = findIndexOfCellTextView(tv);
+        int i = n/COLUMN_COUNT;
+        int j = n%COLUMN_COUNT;
+        tv.setText(String.valueOf(i)+String.valueOf(j));
+        if (tv.getCurrentTextColor() == Color.GRAY) {
+            tv.setTextColor(Color.GREEN);
+            tv.setBackgroundColor(Color.parseColor("lime"));
+        }else {
+            tv.setTextColor(Color.GRAY);
+            tv.setBackgroundColor(Color.LTGRAY);
+        }
     }
 }
